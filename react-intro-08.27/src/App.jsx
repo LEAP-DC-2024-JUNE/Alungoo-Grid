b0da5366dfd1ce4086007c743ab9259af2db2cfe
Alungoo-T <alungoo976@gmail.com>
2024-08-27 22:00:13 -0400

    new folder

---
+++ react-intro-08.27/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="box1">Iphone 6S</div>
+      <div className="box2">Learn More</div>
+      <div className="box3">Our Brand New</div>
+      <div className="box4">Hardly a month passes these days</div>
+      <div className="box5">iCloud</div>
+      <div className="box6">Games ourpasion</div>
+      <div className="box7">MuchBetter Experince</div>
+      <div className="box8">Connect with Twitter</div>
+      <div className="box9">Much MuchBetter Experience</div>
+      <div className="box10">More than just a display</div>
+    </div>
+  );
+}
+
+export default App;
