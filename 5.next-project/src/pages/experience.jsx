2ccab1431b7000c1acc492cc336436bc6006534b
Alungoo-T <alungoo976@gmail.com>
2024-09-02 23:32:17 -0400

    new icons

--- 5.next-project/src/pages/experience.jsx
+++ 5.next-project/src/pages/experience.jsx
@@ -1,22 +1,62 @@
+import Upwork from "@/icons/Upwork";
+
 export default function Experience() {
   return (
     <div>
       <button className="bg-gradient-to-r from-slate-200 to-slate-500">
         Experiences
       </button>
-      <p>Here is a quick summary of my most recent experiences:</p>
-      <div className="flex">
-        <ul>
-          <li>Upwork logo</li>
-          <li> Sr Developer</li>
+      <div>
+        <p>Here is a quick summary of my most recent experiences:</p>
+        <div className="flex">
           <ul>
-            <li>Lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
+            <li>
+              <Upwork />
+            </li>
+            <li> Sr Developer</li>
+            <ul>
+              <li>Lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+            </ul>
+            <li>Nov 2021 - Present</li>
           </ul>
-          <li>Nov 2021 - Present</li>
-        </ul>
+        </div>
+      </div>
+      <div>
+        <div className="flex">
+          <ul>
+            <li>
+              <Upwork />
+            </li>
+            <li> Sr Developer</li>
+            <ul>
+              <li>Lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+            </ul>
+            <li>Nov 2021 - Present</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div className="flex">
+          <ul>
+            <li>
+              <Upwork />
+            </li>
+            <li> Sr Developer</li>
+            <ul>
+              <li>Lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+            </ul>
+            <li>Nov 2021 - Present</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- 5.next-project/src/pages/skills.jsx
+++ 5.next-project/src/pages/skills.jsx
@@ -1,3 +1,14 @@
+import Express from "@/icons/Express";
+import JS from "@/icons/JS";
+import Mongo from "@/icons/Mongo";
+import Next from "@/icons/Next";
+import Node from "@/icons/Node";
+import React from "@/icons/React";
+import Sass from "@/icons/Sass";
+import Socket from "@/icons/Socket";
+// import SQL from "../../public/Tech.png";
+import TS from "@/icons/TS";
+
 export default function Skills() {
   return (
     <div>
@@ -5,8 +16,22 @@ export default function Skills() {
         Skills
       </button>
       <p>The skills, tools and technologies I am really good at:</p>
-      <div className="Logos">
-        <JS />
+      <div>
+        <div>
+          <JS />
+          <TS />
+          <React />
+          <Next />
+          <Node />
+          <Express />
+          <Sass />
+          <Socket />
+        </div>
+        <div>
+          {/* <SQL /> */}
+          <Mongo />
+          <Sass />
+        </div>
       </div>
     </div>
   );
