6b33b857a35e84d01f57a1d8412c254d75b1ae74
Alungoo-T <alungoo976@gmail.com>
2024-10-03 00:22:04 -0400

    Carousel

--- blog-project/src/components/CarouselCard.jsx
+++ blog-project/src/components/CarouselCard.jsx
@@ -1,17 +1,38 @@
+import { Next } from "@/icons/Next";
+import { Prev } from "@/icons/Prev";
+import { useState } from "react";
 export const CarouselCard = ({ img, tags, title, publishDate }) => {
+  const [current, setCurrent] = useState(0);
+  const prev = () => {
+    setCurrent((current) => (current == 0 ? img.length - 1 : current - 1));
+  };
+  const next = () => {
+    setCurrent((current) => (current == img.length - 1 ? 0 : current + 1));
+  };
   return (
-    <div className=" w-full">
-      <div className=" w-[1220px] flex flex-col items-center">
-        <div className=" h-[300px] py-10 bg-pink-300 px-40 relative ">
-          <img src={img} />
-        </div>
-        <div className=" bg-gray-100 w-[600px] h-[250px] absolute left-2 bottom-4 flex flex-col justify-center">
-          <div className=" flex flex-col items-start pl-10 gap-3">
-            <span>{tags}</span>
-            <span>{title}</span>
-            <span>{publishDate}</span>
-          </div>
-        </div>
+    <div className=" relative w-[1220px] overflow-hidden">
+      <div
+        className="transition-transform ease-out duration-500 "
+        style={{ transform: `translateX(-${current * 100}%)` }}
+      >
+        <img src={img} width={1000} height={450} className=" flex" />
+      </div>
+
+      <div className="flex flex-col items-start pl-10 gap-3 bg-gray-50 w-[600px] h-[250px] absolute bottom-16 left-5 flex flex-col justify-center">
+        <span className="absolute bottom-44 bg-blue-100 text-blue-500 text-sm ">
+          {tags}
+        </span>
+        <span className="absolute bottom-20 font-bold text-2xl ">{title}</span>
+        <span className="absolute bottom-10">{publishDate}</span>
+      </div>
+
+      <div className=" flex">
+        <button onClick={prev} className=" hover:bg-white p-2">
+          <Prev />
+        </button>
+        <button onClick={next} className=" hover:bg-white p-2">
+          <Next />
+        </button>
       </div>
     </div>
   );
--- blog-project/src/components/CarouselContainer.jsx
+++ blog-project/src/components/CarouselContainer.jsx
@@ -8,7 +8,7 @@ export const CarouselContainer = () => {
       .then((response) => response.json())
       .then((data) => setArticles(data));
   };
-
+  console.log(articles);
   useEffect(() => {
     fetchData();
   }, []);
@@ -16,13 +16,15 @@ export const CarouselContainer = () => {
     <div>
       {articles.map((article) => {
         return (
-          <div>
-            <CarouselCard
-              img={article.social_image}
-              tags={article.tags}
-              title={article.title}
-              publishDate={article.readable_publish_date}
-            />
+          <div className="  ">
+            <div className=" flex flex-col items-center">
+              <CarouselCard
+                img={article.social_image}
+                tags={article.tags}
+                title={article.title}
+                publishDate={article.readable_publish_date}
+              />
+            </div>
           </div>
         );
       })}
